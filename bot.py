6bd04aee613a19e46073fada10005c0efde8f8f5
ericenlinea-netizen <ericenlinea@gmail.com>
2026-03-15 19:21:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+import os
 from telethon import TelegramClient, events
 
-# CONFIGURACIÓN (la completaremos luego)
-api_id = 35915827
-api_hash = "780bf641de72477aa9330687c3a6ea40"
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+phone = os.environ["PHONE"]
 
-canal = "Allan Bot - Ruleta | 🌎"
+canal = os.environ["CANAL"]
 
 contador_green = 0
 esperando_green = False
@@ -14,24 +15,22 @@ client = TelegramClient('session', api_id, api_hash)
 @client.on(events.NewMessage(chats=canal))
 async def detectar(event):
     global contador_green, esperando_green
-    
+
     texto = event.raw_text.upper()
 
-    # detectar RED
     if "RED" in texto or "❌" in texto:
         esperando_green = True
         contador_green = 0
-        await client.send_message("me", "🚨 ALERTA: SALIÓ RED")
+        await client.send_message("me","🚨 ALERTA: SALIÓ RED")
 
-    # detectar GREEN
     if esperando_green and ("GREEN" in texto or "🟢" in texto):
         contador_green += 1
-        
+
         if contador_green == 3:
-            await client.send_message("me", "✅ SALIERON 3 GREEN DESPUÉS DEL RED")
+            await client.send_message("me","✅ SALIERON 3 GREEN DESPUÉS DEL RED")
             esperando_green = False
             contador_green = 0
 
-client.start()
+client.start(phone)
 print("Bot monitoreando canal...")
 client.run_until_disconnected()
