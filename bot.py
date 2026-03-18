e875edea3c3d704b6958de4bf1f90148e53b6389
ericenlinea-netizen <ericenlinea@gmail.com>
2026-03-18 17:04:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,13 +54,13 @@ async def detectar(event):
             contador_green += 1
             print("GREEN detectado:", contador_green)
 
-            # aviso de 3
-            if contador_green == 3:
-                await enviar("✅ YA VAN 3 GREEN")
+            # aviso de 4
+if contador_green == 4:
+    await enviar("📢 YA VAN 4 GREEN")
 
-            # 🎯 OBJETIVO CUMPLIDO
-            if contador_green >= 5:
-                await enviar("🎯 OBJETIVO CUMPLIDO (5 GREEN)")
+# objetivo cumplido en 6
+if contador_green >= 6:
+    await enviar("🎯 OBJETIVO CUMPLIDO (6 GREEN)")
                 esperando_green = False
                 contador_green = 0
 
